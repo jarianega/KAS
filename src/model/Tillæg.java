62ff585018853a5dae22980311d5c902382b6e9b
ccommerou <ccommerou@gmail.com>
2022-04-26 23:07:26 +0200

    Designklasserettelser og beregnSamletPris metode

---
+++ src/model/Tillæg.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Tillæg {
+    public String navn;
+    public int pris;
+
+    public Tillæg(String navn, int pris) {
+        this.navn = navn;
+        this.pris = pris;
+    }
+
+}
