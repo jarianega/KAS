58be2264876e74ca73e5459264e4dcf9bbf9c470
ccommerou <ccommerou@gmail.com>
2022-04-27 11:37:01 +0200

    Designklasserettelser og beregnSamletPris metode

---
+++ src/gui/Test.java
@@ -0,0 +1,40 @@
+package gui;
+
+import controller.Controller;
+import storage.Storage;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args){
+
+
+       Controller.createKonference("Odense Universitet"),localDate.of(2021,12,18),localDate.of(2021,12,18),Arraylist<Hotel> hoteller,Arraylist<Tillæg> tillæg,1500);
+
+        Controller.createDeltager("Finn Madsen", "Bogfinkevej 420", "København", 88888888, false, LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+        Controller.createDeltager("Niels Petersen","Aggervej 22", "Agger", 21212121, false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+        Controller.createDeltager("Ulla Hansen","Romvej 99","Sønderborg",11223344,false, LocalDate.of(2021,12,18), LocalDate.of(2021,12,19), k1);
+        Controller.createDeltager("Peter Sommer","Nørrebrogade 47", "København", 20202020,false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);)
+        Controller.createDeltager("Lone Jensen","Stjernegade 1","Aarhus",12345678,true,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+        Controller.createDeltager("Niels Petersen","Aggervej 22", "Agger", 21212121, false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+
+        Controller.createTillæg("DVS-WIFI", 75)
+        Controller.createTillæg("HP-Bad", 200)
+        Controller.createTillæg("HP-WIFI",75)
+        Controller.createTillæg("PT-Mad", 100)
+
+        Controller.createHotel("Den Hvide Svane", 1050, 1250,Arraylist<Deltager> deltagere, Arraylist<Tillæg> tillæg,t1);
+        Controller.createHotel("Hotel Phønix", 700, 800, Arraylist<Deltager> deltagere, Arraylist<Tillæg> tillæg, t2+t3);
+        Controller.createHotel("Pension Tusindfryd", 500, 600,Arraylist<Deltager> deltagere, Arraylist<Tillæg> tillæg, t4);
+
+
+
+
+
+
+
+
+    }
+
+}
