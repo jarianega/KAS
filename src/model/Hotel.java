b6329030e79e3d176d2de3a772e80d0241549e46
ccommerou <ccommerou@gmail.com>
2022-04-27 08:58:09 +0200

    Designklasserettelser og beregnSamletPris metode

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -7,15 +7,18 @@ public class Hotel {
     private String hotelnavn;
     private LocalDate dato;
     private int hotelpris;
+    private int dobbeltpris;
     private ArrayList<Deltager> deltagere;
     private ArrayList<Tillæg> tillæg;
 
-    public Hotel(String hotelnavn, LocalDate dato, int hotelpris, ArrayList<Deltager> deltagere, ArrayList<Tillæg> tillæg) {
+    public Hotel(String hotelnavn, LocalDate dato, int hotelpris, int dobbeltpris, ArrayList<Deltager> deltagere, ArrayList<Tillæg> tillæg) {
         this.hotelnavn = hotelnavn;
         this.dato = dato;
         this.hotelpris = hotelpris;
+        this.dobbeltpris = dobbeltpris;
         this.deltagere = deltagere;
         this.tillæg = tillæg;
+
     }
     // getters
 
@@ -24,12 +27,14 @@ public class Hotel {
     }
     public LocalDate getDato() {return dato;}
     public int getHotelpris() {return hotelpris;}
+    public int getDobbeltpris(){return dobbeltpris;}
 
     // setters
 
     public void setNavn(String hotelnavn) {this.hotelnavn = hotelnavn;}
     public void setDato(LocalDate dato) {this.dato = dato;}
     public void setHotelpris(int hotelpris) {this.hotelpris = hotelpris;}
+    public void setDobbeltpris(int dobbeltpris) {this.dobbeltpris = dobbeltpris;}
 
 }
 
