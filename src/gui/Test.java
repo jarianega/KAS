b6a87d36fd96e4346770bef24c8c360216920178
ccommerou <ccommerou@gmail.com>
2022-04-27 12:15:22 +0200

    Designklasserettelser og beregnSamletPris metode

--- src/gui/Test.java
+++ src/gui/Test.java
@@ -13,13 +13,30 @@ public class Test {
        Controller.createKonference("Odense Universitet"),localDate.of(2021,12,18),localDate.of(2021,12,18),Arraylist<Hotel> hoteller,Arraylist<Udflugter> udflugter,1500);
 
         Deltager d1 = Controller.createDeltager("Finn Madsen", "Bogfinkevej 420", "København", 88888888, false, LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
-        d1.setHotel(h1);
+
 
         Deltager d2 = Controller.createDeltager("Niels Petersen","Aggervej 22", "Agger", 21212121, false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+        d2.setHotel(h1);
         Deltager d3 = Controller.createDeltager("Ulla Hansen","Romvej 99","Sønderborg",11223344,false, LocalDate.of(2021,12,18), LocalDate.of(2021,12,19), k1);
-        Deltager d4 = Controller.createDeltager("Peter Sommer","Nørrebrogade 47", "København", 20202020,false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);)
+
+        Deltager d4 = Controller.createDeltager("Peter Sommer","Nørrebrogade 47", "København", 20202020,false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+        d4.setHotel(h1);
+        d4.addTillæg(t1);
+        d4.createLedsager("Mie Sommer");
+
         Deltager d5 = Controller.createDeltager("Lone Jensen","Stjernegade 1","Aarhus",12345678,true,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
-        Deltager d6 = Controller.createDeltager("Niels Petersen","Aggervej 22", "Agger", 21212121, false,LocalDate.of(2021,12,18), LocalDate.of(2021,12,20), k1);
+        d5.setHotel(h1);
+        d5.addTillæg(t1);
+        d5.createLedsager("Jan Madsen");
+
+        Udflugt u1 = Controller.createUdflugt("Byrundtur Odense",localdate.of(2021,12,18),125);
+        Udflugt u2 = Controller.createUdflugt("Egeskov",localdate.of(2021,12,19),75);
+        Udflugt u3 = Controller.createUdflugt("Trapholt Museum, Kolding",localdate.of(2021,12,20),200);
+
+
+
+
+
 
         Tillæg t1 = Controller.createTillæg("DVS-WIFI", 75);
         Tillæg t2 = Controller.createTillæg("HP-Bad", 200);
